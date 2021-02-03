73785781bd07ef83b8d19a9d468661ed2ebc6049
Den107 <deniskrv@gmail.com>
2021-02-03 21:39:02 +0300

    подключил плагины для формы фильтра и начал делать форму фильтра

--- js/main.js
+++ js/main.js
@@ -29,4 +29,13 @@ $(function () {
     prevArrow: '<button class="product-slider__slider-btn product-slider__slider-btnprev"><img src="images/arrow-black-left.svg" alt="arr"></button>',
     nextArrow: '<button class="product-slider__slider-btn product-slider__slider-btnnext"><img src="images/arrow-black-right.svg" alt="arr"></button>',
   });
+
+  //Form
+  $('.filter-style').styler();
+
+  //Drop
+  $('.filter__item-drop').on('click', function () {
+    $(this).toggleClass('filter__item-drop--active');
+    $(this).next().slideToggle(200);
+  });
 });
